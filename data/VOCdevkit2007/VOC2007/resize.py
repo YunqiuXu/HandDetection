ce246469843847adf76e8d0a090231a19c18304f
Yunqiu Xu <yunqiu_xu@163.com>
2017-10-06 19:02:49 +1100

    add zip

--- data/VOCdevkit2007/VOC2007/resize.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-# resize the images to 256 * 480
-import sys, cv2
-imgs = sys.argv[1:]
-for img_name in imgs:
-    img = cv2.imread(img_name)
-    img = cv2.resize(img, (480, 256))
-    cv2.imwrite("JPEGImages/" + img_name, img)
